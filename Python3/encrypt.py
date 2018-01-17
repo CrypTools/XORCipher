ff47fc60d183c787f51f118bff010f8ca005cdbe
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2018-01-17 14:06:39 +1100

    Update encrypt.py

--- Python3/encrypt.py
+++ Python3/encrypt.py
@@ -1,1 +1,14 @@
-nearly finished ...
+def encrypt(initial, key):
+  """ Use (key is an 8 digits binary number) : encrypt("message", "10100100") 
+=> 'éá÷÷åãá'
+  """
+  key = int(''.join(('0b', key)), 2)
+  initial = initial.upper()
+  mylist = []
+  for i in range (len(initial)):
+    mylist.append(int(''.join(('0b', str(format(ord(initial[i]), '08b')))), 2))
+  output = ""
+
+  for i in range(0, len(mylist)):
+    output += str(chr(mylist[i] ^ key))
+  return output
