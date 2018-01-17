b1a509cd3b52d7f7dbb9e15a934487f964b78e7a
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2018-01-17 14:07:02 +1100

    Create decrypt.py

---
+++ Python3/decrypt.py
@@ -0,0 +1,14 @@
+def decrypt(initial, key):
+  """ Use (key is an 8 digits binary number) : decrypt("éá÷÷åãá", "10100100") 
+> 'meSSage'
+  """
+  key = int(''.join(('0b', key)), 2)
+  initial = initial.upper()
+  mylist = []
+  for i in range (len(initial)):
+    mylist.append(int(''.join(('0b', str(format(ord(initial[i]), '08b')))), 2))
+  output = ""
+
+  for i in range(0, len(mylist)):
+    output += str(chr(mylist[i] ^ key))
+  return output
