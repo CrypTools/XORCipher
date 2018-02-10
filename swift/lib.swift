8fb5e48454c205eb7c93ec1f1f7848efd75f550f
Arthur Guiot <arguiot@gmail.com>
2018-02-10 21:47:02 +0800

    Updated XOR Cipher using WIkipedia's method

--- swift/lib.swift
+++ swift/lib.swift
@@ -1,63 +1,38 @@
 /*********************************
-*
-* Use: "Hello World!".encrypt(keyValue: "key")
-* => "IwAVBwpZPAoLBwFY"
-*      ""IwAVBwpZPAoLBwFY".decrypt(keyValue: "key")
-* => "Hello World!"
-*
-*********************************/
+ *
+ * Use: "Hello World!".encrypt([134])
+ * => "Îãêêé¦Ñéôêâ§"
+ *      "U[êqQ¦JQôqZ§".decrypt([134])
+ * => "Hello World!"
+ *
+ *********************************/
 import Foundation
 
 //ascii code
-extension String {
-    var asciiArray: [UInt32] {
-        return unicodeScalars.filter{$0.isASCII}.map{$0.value}
+extension Character
+{
+    func unicodeValue() -> UInt32
+    {
+        let characterString = String(self)
+        let scalars = characterString.unicodeScalars
+        
+        return scalars[scalars.startIndex].value
     }
 }
-extension Character {
-    var asciiValue: UInt32 {
-        return (String(self).unicodeScalars.filter{$0.isASCII}.first?.value)!
-    }
-}
-// Base 64
-extension String {
-
-    func fromBase64() -> String {
-        guard let data = Data(base64Encoded: self) else {
-            return ""
-        }
-
-        return String(data: data, encoding: .utf8)!
-    }
-
-    func toBase64() -> String {
-        return Data(self.utf8).base64EncodedString()
-    }
-}
-
 // main
 extension String {
-    func encrypt(keyValue: String) -> String {
+    func encrypt(_ keyValue: Array<Int>) -> String {
         var out = "";
         let inArray = Array(self);
-        let keyArray = Array(keyValue);
+        let keyArray = keyValue;
         for i in 0...self.count - 1 {
-            let c = Int(inArray[i].asciiValue)
-            let k = Int(keyArray[i % keyValue.count].asciiValue)
-            out += String(Character(UnicodeScalar(Int(c ^ k))!))
-        }
-        return out.toBase64()
-    }
-    func decrypt(keyValue: String) -> String {
-        var out = "";
-        let str = self.fromBase64()
-        let inArray = Array(str)
-        let keyArray = Array(keyValue);
-        for i in 0...str.count - 1 {
-            let c = Int(inArray[i].asciiValue)
-            let k = Int(keyArray[i % keyValue.count].asciiValue)
+            let c = Int(inArray[i].unicodeValue())
+            let k = Int(keyArray[i % keyValue.count])
             out += String(Character(UnicodeScalar(Int(c ^ k))!))
         }
         return out
+    }
+    func decrypt(_ keyValue: Array<Int>) -> String {
+        return self.encrypt(keyValue)
     }
 }
